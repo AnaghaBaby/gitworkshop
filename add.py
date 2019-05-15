269054450110add61778acacb7e2496f14603ce7
ANAGHA M B <20617@fisat>
2019-05-15 04:53:53 -0400

    create a function to add 4 nos

--- add.py
+++ add.py
@@ -1,1 +1,12 @@
-
+def add2(num1,num2):
+	s = num1 + num2
+	return s
+def add3(num1,num2,num3):
+	s = num1 + num2 + num3
+	return s
+def add4(num1,num2,num3,num4):
+	s = num1 + num2 + num3 + num4
+	return s
+print add4(4,5,6,7)
+print add2(4,5)
+print add3(4,5,6)
